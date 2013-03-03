03d9e9d97793259323d2907eb47a49c00dddcadd
Kjuly <kj@kjuly.com>
2013-03-03 10:43:32 +0800

    Add KYUnlockCodeManager/KYUnlockCodeManager.*

---
+++ KYUnlockCodeManager/KYUnlockCodeManager.h
@@ -0,0 +1,13 @@
+//
+//  KYUnlockCodeManager.h
+//  KYUnlockCodeManager
+//
+//  Created by Kjuly on 3/3/13.
+//  Copyright (c) 2013 Kjuly. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KYUnlockCodeManager : NSObject
+
+@end
---
+++ KYUnlockCodeManager/KYUnlockCodeManager.m
@@ -0,0 +1,13 @@
+//
+//  KYUnlockCodeManager.m
+//  KYUnlockCodeManager
+//
+//  Created by Kjuly on 3/3/13.
+//  Copyright (c) 2013 Kjuly. All rights reserved.
+//
+
+#import "KYUnlockCodeManager.h"
+
+@implementation KYUnlockCodeManager
+
+@end
